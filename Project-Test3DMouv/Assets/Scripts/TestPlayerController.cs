6082c7d21c2794ffd61e8882397b191aab6dc5ce
Deleye Arthur <arthurdeleye@gmail.com>
2021-06-14 18:11:17 +0200

    Update Movement Walk

--- Project-Test3DMouv/Assets/Scripts/TestPlayerController.cs
+++ Project-Test3DMouv/Assets/Scripts/TestPlayerController.cs
@@ -1,77 +1,94 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(CapsuleCollider))]
 public class TestPlayerController : MonoBehaviour
 {
+    [SerializeField] private float walkSpeed = 1.0f;
+    [SerializeField] private float runSpeed = 2.0f;
+    [SerializeField] private float rotationSpeed = 2.0f;
 
-    [SerializeField] float walkSpeed = 1.0f;
-    [SerializeField] float runSpeed = 2.0f;
-    [SerializeField] float rotationSpeed = 2.0f;
+    private float currentSpeed = 1.0f;
 
-    float currentSpeed = 1.0f;
-    Rigidbody avatarRB;
-    CapsuleCollider avatarCol;
+    private Rigidbody rb;
+    private CapsuleCollider col;
 
-    Vector2 moveValue;
+    private Vector2 inputValue;
+    private Vector2 moveValue;
 
-    bool isRunning = false;
+    private bool isRunning = false;
 
-
-    void Start()
+    private void Start()
     {
-        avatarRB = GetComponent<Rigidbody>();
-        avatarCol = GetComponent<CapsuleCollider>();
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
     }
 
-    void Update() {
-    }
-    void FixedUpdate() {
-        AvatarMove();
-        AvatarRotation();  
+    private void Update()
+    {
+        GetInput();
     }
 
-    void AvatarMove(){
-        Vector2 moveValue = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
-        if (moveValue.magnitude>1){
-            moveValue.Normalize();
+    private void GetInput()
+    {
+        //Stick Input
+        inputValue = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if (inputValue.magnitude > 1)
+        {
+            inputValue.Normalize();
         }
-        if (moveValue.magnitude>0.5 && Input.GetButton("Running"))
+
+        //IsRunning
+        if (inputValue.magnitude > 0.5 && Input.GetButton("Running"))
         {
             isRunning = true;
-        }else{
+        }
+        else
+        {
             isRunning = false;
         }
+    }
 
+    private void FixedUpdate()
+    {
+        AvatarMove();
+        AvatarRotation();
+    }
 
-        if (isRunning){
+    private void AvatarMove()
+    {
+        //CurrentRunSpeed
+        if (isRunning)
+        {
             currentSpeed = runSpeed;
-        }else{
+        }
+        else
+        {
             currentSpeed = walkSpeed;
         }
 
-        Vector3 avatarOrientation = Vector3.ProjectOnPlane(transform.forward,Vector3.up);
-        Vector2 avatarOrientation2D = new Vector2(avatarOrientation.x,avatarOrientation.z); 
+        Vector3 avatarOrientation = Vector3.ProjectOnPlane(transform.forward, Vector3.up);
 
-        float angle = Mathf.Atan2(moveValue.x,moveValue.y) - Mathf.Atan2(avatarOrientation2D.x,avatarOrientation2D.y);
-        Debug.Log(angle.ToString());
-        Debug.Log(moveValue.ToString());
-        moveValue.x = Mathf.Cos(angle) - Mathf.Sin(angle);
-        moveValue.y = Mathf.Sin(angle) + Mathf.Cos(angle);
-        Debug.Log(moveValue.ToString());
+        Vector2 orient2dVert = new Vector2(avatarOrientation.x, avatarOrientation.z);
+        Vector2 orient2dHori = -Vector2.Perpendicular(orient2dVert);
+
+        moveValue = inputValue.x * orient2dHori + inputValue.y * orient2dVert;
+
+        transform.Translate(
+            moveValue.x * currentSpeed * Time.deltaTime, 
+            0,
+            moveValue.y * currentSpeed * Time.deltaTime);
 
 
+        //Debug
+        Debug.DrawRay(Vector3.zero, new Vector3(orient2dVert.x, 0, orient2dVert.y), Color.red);
+        Debug.DrawRay(Vector3.zero, new Vector3(orient2dHori.x, 0, orient2dHori.y), Color.green);
 
-        transform.Translate(moveValue.x*currentSpeed*Time.deltaTime,0,moveValue.y*currentSpeed*Time.deltaTime);
-        
-        
+        Debug.DrawRay(Vector3.zero, new Vector3(moveValue.x, 0, moveValue.y), Color.yellow);
+
     }
 
-    void AvatarRotation(){
-
-
+    private void AvatarRotation()
+    {
     }
-
 }
